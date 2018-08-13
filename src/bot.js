6ab7c6c316b8f73493884a5d1d3c141e57dd2372
Harvey Li <aide0740@gmail.com>
2018-08-14 01:26:10 +0800

    service

--- src/bot.js
+++ src/bot.js
@@ -6,15 +6,15 @@ const tweet = require('./tweet');
 
 console.log('Bot starting...');
 // Welcome sentences when first running.
-var welcome = "姐姐大人们好~这里是Bangumi Bot，会在每一季新番结束的时候根据Bangumi.tv上的口碑给大家提供补番推荐，虽然一年只能见四次，但是Bot会珍惜的！\n#BangumiBot"
+// var welcome = "姐姐大人们好~这里是Bangumi Bot，会在每一季新番结束的时候根据Bangumi.tv上的口碑给大家提供补番推荐，虽然一年只能见四次，但是Bot会珍惜的！\n#BangumiBot"
 
-var d =new Date();
-d.setMonth(7);
-d.setDate(1);
-d.setFullYear(2018);
+// var d =new Date();
+// d.setMonth(7);
+// d.setDate(1);
+// d.setFullYear(2018);
 
-tweet(welcome);
-getTopBangumi(generateText,d);
+// tweet(welcome);
+// getTopBangumi(generateText,d);
 
 getTopBangumi(generateText);
 
--- src/getTopBangumi.js
+++ src/getTopBangumi.js
@@ -4,13 +4,14 @@ const getTopBangumi = (dosth,date) => {
         var d = new Date();    
     else 
         d = date;
-    console.log("Today is " + d);
     if(d.getDate()==1&&(d.getMonth()==1||d.getMonth()==4||d.getMonth()==7||d.getMonth()==10)){
+        console.log("New Season...Fetching List...\n")
         year = d.getFullYear();
         month = d.getMonth();
         questString = "http://bangumi.tv/anime/browser/tv/airtime/"+year+"-"+month+"?sort=rank";
         request(questString, function(error,response,body){
             if(!error&&response.statusCode==200){
+                console.log("Feching Success.");
                 dosth(month,body,3);
             }else{
                 console.log(error);
